b7ec30677849c0cab811d80ad1c8957e72c768dd
subhash pal <palsubhash046@gmail.com>
2026-05-12 15:44:43 +0530

    Added NextAuth authentication setup

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,7 @@
+import { handlers } from "@/auth" 
+export const { GET, POST } = handlers
+
+
+//IMPORTENT>>>
+// Iska use Auth.js ke authentication routes handle karne ke liye karte hain.
+// Ye login, logout, session aur OAuth requests ko automatically manage karta hai backend side par.
