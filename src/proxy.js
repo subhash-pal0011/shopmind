f001387c473f4f7cbad80de9815e4ad4f5e70462
subhash pal <palsubhash046@gmail.com>
2026-06-07 19:12:09 +0530

    complete setup proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,1 +1,38 @@
-export { auth as proxy } from "@/auth"
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+       const { pathname } = req.nextUrl;
+
+       if (req.auth && pathname === "/register") {
+              return NextResponse.redirect(new URL("/", req.url));
+       }
+
+       const publicRoutes = [
+              "/register",
+              "/api/auth",
+              "/favicon.ico",
+              "/_next",
+       ];
+
+       // Allow public routes
+       if (publicRoutes.some((path) => pathname.startsWith(path))) {
+              return NextResponse.next();
+       }
+
+       // If user is not logged in
+       if (!req.auth) {
+              return NextResponse.redirect(
+                     new URL("/register", req.url)
+              );
+       }
+       //Invalid route → redirect home bana hii
+
+       return NextResponse.next();
+});
+
+export const config = {
+       matcher: [
+              "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|webp|svg)$).*)",
+       ],
+};
