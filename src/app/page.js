577cfc580f7fb3bc00c191c1de729ea81bd9dcb1
subhash pal <palsubhash046@gmail.com>
2026-06-13 21:24:13 +0530

    find user for role

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,32 @@
+import { auth } from "@/auth";
+import EditRoleAurPhone from "@/component/EditRoleAurPhone";
+import Navbar from "@/component/Navbar";
+import connectDb from "@/lib/connectDb";
+import User from "@/model/user";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  await connectDb();
+
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    redirect("/register");
+  }
+
+  const findUser = await User.findById(session.user.id);
+
+  if (!findUser) {
+    redirect("/register");
+  }
+
+  const isExists = !findUser?.phone || !findUser?.userRole || (!findUser?.phone && !findUser?.userRole === "user");
+
+  if (isExists) return <EditRoleAurPhone />;
+
   return (
-   <div>fgfgfgfd</div>
+    <div className="bg-gray-500">
+      <Navbar />
+    </div>
   );
 }
