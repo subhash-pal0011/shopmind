de7bcb6c1ae869a586bbada19d2604d3464e38da
subhash pal <palsubhash046@gmail.com>
2026-06-15 21:48:21 +0530

    chenge rest api http method

--- src/app/api/currentUser/route.js
+++ src/app/api/currentUser/route.js
@@ -3,35 +3,51 @@ import connectDb from "@/lib/connectDb";
 import User from "@/model/user";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function GET() {
        try {
               await connectDb();
 
               const session = await auth();
 
-              if (!session || session?.user?.email) {
+              if (!session?.user?.email) {
                      return NextResponse.json(
-                            { success: false, message: "User not authenticated" },
+                            {
+                                   success: false,
+                                   message: "User not authenticated",
+                            },
                             { status: 401 }
-                     )
+                     );
               }
 
-              const findUser = await User.findOne({ email: session?.user?.email }).select("-password");
+              const findUser = await User.findOne({
+                     email: session.user.email,
+              }).select("-password");
 
               if (!findUser) {
                      return NextResponse.json(
-                            { success: false, message: "User not found" },
+                            {
+                                   success: false,
+                                   message: "User not found",
+                            },
                             { status: 404 }
                      );
               }
 
               return NextResponse.json(
-                     {success:true, data:findUser},
-                     {status:404}
-              )
+                     {
+                            success: true,
+                            data: findUser,
+                     },
+                     { status: 200 }
+              );
        } catch (error) {
+              console.error("Get self user error:", error);
+
               return NextResponse.json(
-                     { success: false, message: `Get self user error ${error.message}` },
+                     {
+                            success: false,
+                            message: `Get self user error: ${error.message}`,
+                     },
                      { status: 500 }
               );
        }
