94a0444377c18dd1e16cf6c16b9fc0dabbd2394f
subhash pal <palsubhash046@gmail.com>
2026-06-15 21:46:35 +0530

    correct and optmize code

--- src/hooks/CurrentUser.js
+++ src/hooks/CurrentUser.js
@@ -1,25 +1,33 @@
+"use client";
+
+import { setUserData } from "@/redux/userSlice";
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { toast } from "sonner";
 
 const CurrentUser = () => {
-       
        const dispatch = useDispatch();
 
        useEffect(() => {
               const getCurrentUser = async () => {
                      try {
                             const res = await axios.get("/api/currentUser");
+
                             if (res.data.success) {
-                                   toast.success(res.data.message)
+                                   dispatch(setUserData(res.data.data));
                             }
                      } catch (error) {
-                            console.log("get current-user error:", error);
+                            console.log(
+                                   "Get current user error:",
+                                   error.response?.data || error.message
+                            );
                      }
-              }
-              getCurrentUser()
-       }, [dispatch])
-}
+              };
 
-export default CurrentUser
+              getCurrentUser();
+       }, [dispatch]);
+
+       return null;
+};
+
+export default CurrentUser;
