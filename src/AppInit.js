1d7250af0edefc46ec36faefdda0c3a35944c5db
subhash pal <palsubhash046@gmail.com>
2026-06-16 12:06:16 +0530

    add socket conect func call and disconect

--- src/AppInit.js
+++ src/AppInit.js
@@ -1,9 +1,22 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import CurrentUser from './hooks/CurrentUser'
 import AllVendorUser from './hooks/AllVendorUser'
+import { disconnectSocket, socketConnection } from './lib/socketConnection'
+import { useSelector } from 'react-redux'
 
 const AppInit = ({ children }) => {
+       const userData = useSelector((state) => state.user.userData);
+       const userId = userData?._id;
+
+       useEffect(() => {
+              if (userId) {
+                     socketConnection();
+              } else {
+                     disconnectSocket();
+              }
+       }, [userId]);
+
        return (
               <>
                      <CurrentUser />
