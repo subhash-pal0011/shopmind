63238f8e45fbe9916a550ab70039236e0002c233
subhash pal <palsubhash046@gmail.com>
2026-06-16 10:51:27 +0530

    feat: add vendor user data initialization hook

---
+++ src/hooks/AllVendorUser.js
@@ -0,0 +1,32 @@
+"use client";
+import { setVendorUserData } from "@/redux/allVendorUserSLice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+const AllVendorUser = () => {
+       const dispatch = useDispatch();
+
+       useEffect(() => {
+              const getAllVendorUser = async () => {
+                     try {
+                            const res = await axios.get("/api/vendor/allVendorUser");
+
+                            if (res.data.success) {
+                                   dispatch(setVendorUserData(res.data.data));
+                            }
+                     } catch (error) {
+                            console.log(
+                                   "Get current user error:",
+                                   error.response?.data || error.message
+                            );
+                     }
+              };
+
+              getAllVendorUser();
+       }, [dispatch]);
+
+       return null;
+};
+
+export default AllVendorUser;
