451bdd7ad254c0eeebb3c59c079011a45f68a458
subhash-pal0011 <palsubhash046@gmail.com>
2026-07-05 14:21:23 +0530

    correct and clean route for get-vendors and real time get-vendors

--- src/component/Admin/VendorApproval.jsx
+++ src/component/Admin/VendorApproval.jsx
@@ -1,16 +1,17 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import axios from "axios";
 import { motion } from "motion/react";
-import { socketConnection } from "@/lib/socketConnection";
+import { socketConnection, disconnectSocket } from "@/lib/socketConnection";
+import { toast } from "sonner";
+import { CircleCheckBig } from "lucide-react";
 
 const VendorApproval = () => {
-
-  // const allVendorUserData = useSelector((state) => state.vendorUser.vendorUserData);
-
   const [allVendorUserData, setAllVendorUserData] = useState([]);
+  const [statusApproveLoding, setStatusApproveLoding] = useState(false)
 
+
+  // GET-VENDORS
   const getVendors = async () => {
     try {
       const res = await axios.get("/api/vendor/allVendorUser");
@@ -19,33 +20,55 @@ const VendorApproval = () => {
         setAllVendorUserData(res.data.data);
       }
     } catch (err) {
-      console.log("Fetch error:", err.message);
+      console.log(err);
     }
   };
 
+  // REAL TIME GET-VENDORS
   useEffect(() => {
     getVendors();
 
     const socket = socketConnection();
 
-    //socket connect check
     socket.on("connect", () => {
-      console.log("socket connected:", socket.id);
+      console.log("Socket Connected:", socket.id);
     });
 
-    //REAL-TIME LISTENER
-    socket.on("new-approvel-for-notification", (notifi) => {
-      // console.log("realtime event:", notifi);
-
-      getVendors();
+    socket.on("new-approvel-for-notification", (vendors) => {
+      console.log("Realtime Vendors:", vendors);
+      setAllVendorUserData(vendors);
     });
 
     return () => {
-      socket.off("connect");
       socket.off("new-approvel-for-notification");
-      socket.disconnect();
+      disconnectSocket();
     };
   }, []);
+
+
+  // STATUS APPROVED API FUNCTION.
+  const handleApprove = async (vendorId) => {
+    try {
+      setStatusApproveLoding(true);
+
+      const res = await axios.post("/api/vendor/updateVendorStatus", {
+        vendorId,
+        status: "approved",
+      });
+
+      if (res.data.success) {
+        toast.success(res.data.message);
+
+        setAllVendorUserData((prev) =>
+          prev.filter((item) => item._id !== vendorId)
+        );
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Server Error");
+    } finally {
+      setStatusApproveLoding(false);
+    }
+  };
 
   return (
     <div className="w-full min-h-screen bg-gray-200 p-5 md:px-10">
@@ -53,9 +76,8 @@ const VendorApproval = () => {
         Vendor Approval Requests
       </h3>
 
-      {allVendorUserData?.length > 0 ? (
+      {allVendorUserData.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
-
           {allVendorUserData.map((vendor, index) => (
 
             <motion.div key={vendor._id}
@@ -117,8 +139,16 @@ const VendorApproval = () => {
               </div>
 
               <div className="flex flex-wrap gap-3 mt-5">
-                <button className="flex-1 min-w-32 bg-green-500 hover:bg-green-600 text-white py-2 rounded-lg transition cursor-pointer">
-                  Approve
+                <button
+                  onClick={() => handleApprove(vendor._id)}
+                  disabled={statusApproveLoding}
+                  className="flex-1 min-w-32 bg-green-500 hover:bg-green-600 disabled:bg-green-400 text-white py-2 rounded-lg flex items-center justify-center cursor-pointer"
+                >
+                  {statusApproveLoding ? (
+                    <img src="/Loading.gif" alt="Loading" className="w-6 h-6" />
+                  ) : (
+                    "Approve"
+                  )}
                 </button>
 
                 <button className="flex-1 min-w-32 bg-red-500 hover:bg-red-600 text-white py-2 rounded-lg transition cursor-pointer">
@@ -129,10 +159,24 @@ const VendorApproval = () => {
           ))}
         </div>
       ) : (
-        <div className="p-10 text-center text-gray-500 items-center justify-center flex flex-col">
+        <div className="flex  w-full items-center justify-center">
+          <div className="w-full max-w-lg rounded-3xl  text-center mt-10 md:mt-20 ">
 
-          <p className="mt-20">No vendor requests found</p>
-          <img src="/Loading.gif" className="h-30" />
+            <div className="mx-auto flex h-24 w-24 sm:h-28 sm:w-28 items-center justify-center rounded-full bg-blue-50">
+              <CircleCheckBig className="h-12 w-12 sm:h-16 sm:w-16 text-blue-600" />
+            </div>
+
+            <h2 className="mt-6 text-2xl sm:text-3xl font-bold text-gray-800">
+              No Pending Vendors
+            </h2>
+
+            <p className="mt-3 text-sm sm:text-base leading-7 text-gray-500">
+              Great job! 🎉 There are currently no vendor approval requests.
+              New vendor applications will automatically appear here as soon as
+              they are submitted.
+            </p>
+
+          </div>
         </div>
       )}
     </div>
