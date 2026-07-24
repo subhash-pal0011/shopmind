43c91d3dce3cc38a971a3291e9948d0d807db254
Subhash Pal <palsubhash046@gmail.com>
2026-07-24 11:18:41 +0530

    correct and complete nav

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -16,124 +16,136 @@ import { LuLayoutGrid } from "react-icons/lu";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { RiListUnordered } from "react-icons/ri";
 import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
 
 const Navbar = ({ user }) => {
-
   const [menuShow, setMenuShow] = useState(false);
   const [sideBar, setSideBar] = useState(false);
   const router = useRouter();
 
-  // const menuItems = [
-  //   {
-  //     icon: <LuCircleUserRound size={15} />,
-  //     label: "Profile",
-  //     onClick: () => router.push("/edit-profile"),
-  //   },
-  //   {
-  //     icon: <PiSignIn />,
-  //     label: "Sign In",
-  //   },
-  //   {
-  //     icon: <PiSignOutFill color="red" />,
-  //     label: "Sign Out",
-  //     danger: true,
-  //   },
-  // ];
+  const handleMenuItemClick = (item) => {
+    setMenuShow(false);
+    setSideBar(false);
+    item.onClick?.();
+  };
 
   const menuItems = [
-    {
+    user && {
       icon: <LuCircleUserRound size={15} />,
       label: "Profile",
       onClick: () => router.push("/edit-profile"),
     },
-    {
+    !user && {
       icon: <PiSignIn />,
       label: "Sign In",
       onClick: () => router.push("/register"),
     },
-    {
+    user && {
       icon: <PiSignOutFill color="red" />,
       label: "Sign Out",
       onClick: () => signOut(),
     },
-  ];
+  ].filter(Boolean);
 
-  const element = sideBar ? createPortal(
-    <AnimatePresence>
-      <motion.div
-        key="menu"
-        initial={{ x: "100%" }}
-        animate={{ x: 0 }}
-        className="w-60 inset-0 z-50 border h-screen fixed bg-gray-100 p-5 rounded-tr-xl rounded-br-xl space-y-5">
+  const element = sideBar
+    ? createPortal(
+        <AnimatePresence>
+          <motion.div
+            key="menu"
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            className="w-60 inset-0 z-50 border h-screen fixed bg-gray-100 p-5 rounded-tr-xl rounded-br-xl space-y-5"
+          >
+            <motion.div
+              initial={{ y: -40, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.5 }}
+              className="flex gap-1.5 items-center"
+            >
+              <GoHome />
+              <p>Home</p>
+            </motion.div>
 
+            <motion.div
+              initial={{ y: -40, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.7 }}
+              className="flex gap-1.5 items-center"
+            >
+              <LuLayoutGrid />
+              <p>Cotegories</p>
+            </motion.div>
 
-        <motion.div initial={{ y: -40, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 0.5 }}
-          className="flex gap-1.5 items-center">
-          <GoHome />
-          <p>Home</p>
-        </motion.div>
+            <motion.div
+              initial={{ y: -40, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.9 }}
+              className="flex gap-1.5 items-center"
+            >
+              <MdOutlineShoppingBag />
+              <p>Shop</p>
+            </motion.div>
 
-        <motion.div initial={{ y: -40, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 0.7 }}
-          className="flex gap-1.5 items-center">
-          <LuLayoutGrid />
-          <p>Cotegories</p>
-        </motion.div>
+            <motion.div
+              initial={{ y: -40, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 1.1 }}
+              className="flex gap-1.5 items-center"
+            >
+              <RiListUnordered />
+              <p>Order</p>
+            </motion.div>
 
-        <motion.div initial={{ y: -40, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 0.9 }}
-          className="flex gap-1.5 items-center">
-          <MdOutlineShoppingBag />
-          <p>Shop</p>
-        </motion.div>
+            <motion.div
+              onClick={() => {
+                router.push("/edit-profile");
+                setSideBar(false);
+              }}
+              initial={{ y: -40, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 1.3 }}
+              className="flex gap-1.5 items-center cursor-pointer"
+            >
+              <LuCircleUserRound />
+              <p>Profile</p>
+            </motion.div>
 
-        <motion.div initial={{ y: -40, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 1.1 }}
-          className="flex gap-1.5 items-center">
-          <RiListUnordered />
-          <p>Order</p>
-        </motion.div>
+            <motion.div
+              onClick={() => {
+                router.push("/register");
+                setSideBar(false);
+              }}
+              initial={{ y: -40, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 1.4 }}
+              className="flex gap-1.5 items-center cursor-pointer"
+            >
+              <PiSignIn />
+              <p>Login</p>
+            </motion.div>
 
-        <motion.div initial={{ y: -40, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 1.3 }}
-          className="flex gap-1.5 items-center">
-          <LuCircleUserRound />
-          <p>Profile</p>
-        </motion.div>
-
-        <motion.div initial={{ y: -40, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 1.4 }}
-          className="flex gap-1.5 items-center">
-          <PiSignIn />
-          <p>Login</p>
-        </motion.div>
-
-        <motion.div onClick={() => signOut()}
-          initial={{ y: -40, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 1.6 }}
-          className="flex gap-1.5 items-center text-red-500">
-          <PiSignIn />
-          <p>Sign Out</p>
-        </motion.div>
-
-      </motion.div>
-    </AnimatePresence>,
-    document.body
-  ) : null
+            <motion.div
+              onClick={() => {
+                signOut();
+                setSideBar(false);
+              }}
+              initial={{ y: -40, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 1.6 }}
+              className="flex gap-1.5 items-center text-red-500 cursor-pointer"
+            >
+              <PiSignOutFill />
+              <p>Sign Out</p>
+            </motion.div>
+          </motion.div>
+        </AnimatePresence>,
+        document.body,
+      )
+    : null;
 
   return (
     <nav className="sticky top-0 z-50 w-full bg-white shadow-md px-4 md:px-8 py-4 md:py-2">
       <div className="max-w-7xl mx-auto flex items-center justify-around">
-
-        {/* Logo */}
         <div>
           <Image
             src="/logo-2.png"
@@ -145,9 +157,7 @@ const Navbar = ({ user }) => {
         </div>
 
         <div className="flex md:gap-20">
-          {/* Desktop Menu */}
-
-          {user.userRole === "user" &&
+          {user.userRole === "user" && (
             <div className="hidden lg:flex items-center gap-8 text-sm">
               {["Home", "Categories", "Shop", "Other"].map((item) => (
                 <p
@@ -175,12 +185,10 @@ const Navbar = ({ user }) => {
                 </p>
               ))}
             </div>
-          }
+          )}
 
-          {/* Icons */}
-          {user.userRole === "user" ?
+          {user.userRole === "user" ? (
             <div className="flex items-center gap-4 sm:gap-6 md:gap-8 lg:gap-8">
-
               <IoSearchOutline
                 size={20}
                 className="w-5 h-5 sm:w-7 sm:h-6 md:w-7 md:h-6 lg:w-10 lg:h-6 cursor-pointer hover:scale-110 hover:text-blue-500 transition-all duration-200 text-gray-600"
@@ -191,9 +199,7 @@ const Navbar = ({ user }) => {
                 className="w-5 h-4 sm:w-7 sm:h-5 md:w-7 md:h-6 lg:w-7 lg:h-5 cursor-pointer hover:scale-110 hover:text-blue-500 transition-all text-gray-600 duration-200"
               />
 
-              <LuShoppingCart
-                className="w-5 h-4 sm:w-7 sm:h-5 md:w-7 md:h-6 lg:w-10 lg:h-5 cursor-pointer hover:scale-110 hover:text-blue-500 transition-all duration-200 text-gray-600"
-              />
+              <LuShoppingCart className="w-5 h-4 sm:w-7 sm:h-5 md:w-7 md:h-6 lg:w-10 lg:h-5 cursor-pointer hover:scale-110 hover:text-blue-500 transition-all duration-200 text-gray-600" />
 
               <div className="relative">
                 <LuCircleUserRound
@@ -204,25 +210,29 @@ const Navbar = ({ user }) => {
 
                 <AnimatePresence>
                   {menuShow && (
-                    <motion.div initial={{ opacity: 0, y: -10 }}
+                    <motion.div
+                      initial={{ opacity: 0, y: -10 }}
                       animate={{ opacity: 1, y: 0 }}
                       exit={{ opacity: 0, y: -10, ease: "ease-in" }}
-                      transition={{ duration: 0.20, ease: "easeOut" }}
-                      className="absolute right-0 top-10 w-36 bg-white shadow-lg rounded-md border py-2 z-50 p-1">
-
+                      transition={{ duration: 0.2, ease: "easeOut" }}
+                      className="absolute right-0 top-10 w-36 bg-white shadow-lg rounded-md border py-2 z-50 p-1"
+                    >
                       {menuItems.map((item, index) => (
                         <div
                           key={index}
-                          onClick={() => item.onClick?.()}
+                          onClick={() => handleMenuItemClick(item)}
                           className="p-2 hover:bg-gray-100 cursor-pointer transition-all flex items-center gap-1 rounded"
                         >
                           {item.icon}
-                          <p className={`text-sm ${item.label === "Sign Out" ? "text-red-500" : ""}`}>
+                          <p
+                            className={`text-sm ${
+                              item.label === "Sign Out" ? "text-red-500" : ""
+                            }`}
+                          >
                             {item.label}
                           </p>
                         </div>
                       ))}
-
                     </motion.div>
                   )}
                 </AnimatePresence>
@@ -230,17 +240,17 @@ const Navbar = ({ user }) => {
 
               {sideBar ? (
                 <RxCross2
-                  onClick={() => setSideBar(prev => !prev)}
+                  onClick={() => setSideBar((prev) => !prev)}
                   className="w-6 h-5 sm:w-6 sm:h-5 md:w-7 md:h-6 lg:hidden cursor-pointer text-red-500"
                 />
               ) : (
                 <HiMiniBars3
-                  onClick={() => setSideBar(prev => !prev)}
+                  onClick={() => setSideBar((prev) => !prev)}
                   className="w-6 h-4 sm:w-6 sm:h-5 md:w-7 md:h-6 text-gray-600 lg:hidden cursor-pointer"
                 />
               )}
             </div>
-            :
+          ) : (
             <div className="flex gap-8">
               <FiPhoneCall
                 size={30}
@@ -254,43 +264,38 @@ const Navbar = ({ user }) => {
                   className="w-4 h-5 sm:w-7 sm:h-5 md:w-5 md:h-5 lg:w-7 lg:h-5 cursor-pointer hover:scale-110 hover:text-blue-500 transition-all duration-200 text-gray-600"
                 />
 
-
                 <AnimatePresence>
                   {menuShow && (
-                    <motion.div initial={{ opacity: 0, y: -10 }}
+                    <motion.div
+                      initial={{ opacity: 0, y: -10 }}
                       animate={{ opacity: 1, y: 0 }}
                       exit={{ opacity: 0, y: -10, ease: "ease-in" }}
-                      transition={{ duration: 0.20, ease: "easeOut" }}
-                      className="absolute right-0 top-10 w-36 bg-white shadow-lg rounded-md border py-2 z-50 p-1">
-
+                      transition={{ duration: 0.2, ease: "easeOut" }}
+                      className="absolute right-0 top-10 w-36 bg-white shadow-lg rounded-md border py-2 z-50 p-1"
+                    >
                       {menuItems.map((item, index) => (
                         <div
                           key={index}
-                          onClick={() => {
-                            if (item.label === "Sign Out") {
-                              signOut();
-                            }
-                          }}
+                          onClick={() => handleMenuItemClick(item)}
                           className="p-2 hover:bg-gray-100 cursor-pointer transition-all flex items-center gap-1 rounded"
                         >
                           {item.icon}
 
                           <p
-                            className={`text-sm ${item.label === "Sign Out" ? "text-red-500" : ""
-                              }`}
+                            className={`text-sm ${
+                              item.label === "Sign Out" ? "text-red-500" : ""
+                            }`}
                           >
                             {item.label}
                           </p>
                         </div>
                       ))}
-
                     </motion.div>
                   )}
                 </AnimatePresence>
               </div>
             </div>
-          }
-
+          )}
         </div>
       </div>
       {element}
