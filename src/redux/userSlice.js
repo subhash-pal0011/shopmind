5e96192c9708fcb049acf37ef6c2c85fc726cdad
Subhash Pal <palsubhash046@gmail.com>
2026-08-10 16:04:29 +0530

    add updated data for user like img name etc

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,18 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit'
+// import { createSlice } from '@reduxjs/toolkit'
+
+// export const userSlice = createSlice({
+//        name: 'user',
+//        initialState: {
+//               userData:null
+//        },
+//        reducers: {
+
+//               setUserData: (state, action) => {
+//                      state.userData = action.payload
+//               },
+//        },
+// })
+
+// export const { setUserData } = userSlice.actions
+
+// export default userSlice.reducer
+
+import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
-       name: 'user',
-       initialState: {
-              userData:null
-       },
-       reducers: {
-              
-              setUserData: (state, action) => {
-                     state.userData = action.payload
-              },
-       },
-})
+  name: "user",
 
-export const { setUserData } = userSlice.actions
+  initialState: {
+    userData: null,
+  },
 
-export default userSlice.reducer
+  reducers: {
+    // Set complete user data
+    setUserData: (state, action) => {
+      state.userData = action.payload;
+    },
+
+    // Update existing user data
+    updateUser: (state, action) => {
+      if (state.userData) {
+        state.userData = {
+          ...state.userData,
+          ...action.payload,
+        };
+      }
+    },
+  },
+});
+
+export const { setUserData, updateUser} = userSlice.actions;
+
+export default userSlice.reducer;
