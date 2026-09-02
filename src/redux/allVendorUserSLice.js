e3c7cca02f3aa3731d1f67a3059f73daf721fd73
Subhash Pal <palsubhash046@gmail.com>
2026-09-02 15:09:07 +0530

    add allProductForUser

--- src/redux/allVendorUserSLice.js
+++ src/redux/allVendorUserSLice.js
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const allVendorUserSlice = createSlice({
+
   name: "vendorUser",
 
   initialState: {
     vendorUserData: [],
     allVendorProduct: [],
+    allProductForUser : [],
   },
 
   reducers: {
@@ -16,8 +18,12 @@ export const allVendorUserSlice = createSlice({
     setAllVendorProduct: (state, action) => {
       state.allVendorProduct = action.payload;
     },
+
+    setallProductForUser: (state, action) => {
+      state.allProductForUser = action.payload;
+    },
   },
 });
 
-export const {setVendorUserData, setAllVendorProduct} = allVendorUserSlice.actions;
+export const {setVendorUserData, setAllVendorProduct , setallProductForUser} = allVendorUserSlice.actions;
 export default allVendorUserSlice.reducer;
