0cebcbf6d0989d447d1fb2d5c7d68c47b832ff89
Subhash Pal <palsubhash046@gmail.com>
2026-09-02 15:08:35 +0530

    create page

--- src/component/User/UserDashBoard.jsx
+++ src/component/User/UserDashBoard.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Slider from './Slider'
 import Categoreis from './Categoreis'
+import ProductPage from './ProductPage'
+
+
 
 const UserDashBoard = () => {
   return (
     <div className='py-1 bg-gray-200'>
       <Slider />
       <Categoreis />
+      <ProductPage />
     </div>
   )
 }
