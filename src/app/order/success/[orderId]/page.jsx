38b7a8f0dc4f68732934e3a9a76f9055e10aae76
Subhash Pal <palsubhash046@gmail.com>
2026-09-05 16:57:33 +0530

    add order-success page

---
+++ src/app/order/success/[orderId]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen text-2xl font-semibold'>
+        sucess page
+    </div>
+  )
+}
+
+export default page
